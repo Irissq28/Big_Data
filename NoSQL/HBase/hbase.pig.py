7996b4b99407711e109a9f76cfed96d8c245e569
Irissq28 <anantjjz@gmail.com>
2019-03-01 22:48:14 -0800

    NoSQL added

---
+++ NoSQL/HBase/hbase.pig.py
@@ -0,0 +1,10 @@
+users = LOAD '/user/maria_dev/ml-100k/u.user' 
+USING PigStorage('|') 
+AS (userID:int, age:int, gender:chararray, occupation:chararray, zip:int);
+
+#userID as primary key
+#userinfo as col family
+
+STORE users INTO 'hbase://users' 
+USING org.apache.pig.backend.hadoop.hbase.HBaseStorage (
+'userinfo:age,userinfo:gender,userinfo:occupation,userinfo:zip');
